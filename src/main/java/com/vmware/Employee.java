51b412c3091fb79ce76af7ec3806c888cc79b514
kareem Samsondeen <engusr16@VIEW-TRNG-010.vmware.com>
2014-10-03 11:58:48 -0700

    Added Employee  and EmployeeTest code refactor

--- src/main/java/com/vmware/Employee.java
+++ src/main/java/com/vmware/Employee.java
@@ -6,6 +6,17 @@ import java.util.regex.Pattern;
 public class Employee {
    private String name;
    private String ssn;
+   private Pattern pattern;
+   
+   public Employee(Pattern pattern){
+	   
+	   this.pattern=pattern;
+   }
+   // public Employee(){
+    	
+   // 	pattern= Pattern.compile("\\d{3}-\\d{2}-\\d{4}");
+    		
+  //  }
    
    public void setName(String name) {
       this.name = name;
@@ -16,9 +27,9 @@ public class Employee {
    }
 
 public void setSocialSecurityNumber(String ssn) {
-	Pattern pattern = Pattern.compile("\\d{3}-\\d{2}-\\d{4}");
-	Matcher matcher = pattern.matcher(ssn);		
-	if(!(matcher.matches()))
+	//Matcher matcher = pattern.matcher(ssn);
+	//if(!(matcher.matches()))
+		if(!(pattern.matcher(ssn).matches()))
     throw new IllegalArgumentException(ssn + " is not a valid social security number");
 	this.ssn=ssn;	
 }
--- src/test/java/com/vmware/EmployeeTest.java
+++ src/test/java/com/vmware/EmployeeTest.java
@@ -2,13 +2,27 @@ package com.vmware;
 
 import static org.testng.Assert.*;
 
+import java.util.regex.Pattern;
+
+import org.junit.Before;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class EmployeeTest {
 
+	private Employee employee;
+	
+	@BeforeMethod
+	public void setUp(){
+		System.out.println("before method test is create here");
+		employee = new Employee(Pattern.compile("\\d{3}-\\d{2}-\\d{4}"));
+	}
+
+	
 	@Test
 	public void createEmployeeAndGetName() {
-		Employee employee = new Employee();
+		//Employee employee = new Employee();
 		String name = "Ricardo Montalban";
 		employee.setName(name);
 		assertEquals(employee.getName(), name);
@@ -17,7 +31,7 @@ public class EmployeeTest {
 	@Test
 	public void addSocialSecurityNumber(){
 		
-		Employee employee = new Employee();
+		//Employee employee = new Employee();
 		employee.setSocialSecurityNumber("123-45-6789");
 		assertEquals(employee.getSocialSecurityNumber(), "123-45-6789");
 	}
@@ -25,7 +39,7 @@ public class EmployeeTest {
 	@Test
 	public void addAnotherSocialSecurityNumber(){
 		
-		Employee employee = new Employee();
+		//Employee employee = new Employee();
 		employee.setSocialSecurityNumber("123-45-6708");
 		assertEquals(employee.getSocialSecurityNumber(), "123-45-6708");
 	}
@@ -37,7 +51,7 @@ public class EmployeeTest {
 	public void fix123457_SocialSecurityNumberMustBeACertainFormat(){
 		
 		String badSSN ="RamLikesThe49ers";
-		Employee employee = new Employee();
+	//	Employee employee = new Employee();
 		try{
 			employee.setSocialSecurityNumber(badSSN);
 			fail("This Line Should never been reach");
