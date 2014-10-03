53bc1ea65dfacf5b38b207707dea9c4d2363dc19
kareem Samsondeen <engusr16@VIEW-TRNG-010.vmware.com>
2014-10-03 10:54:01 -0700

    Added Employee  and EmployeeTest

--- src/main/java/com/vmware/Employee.java
+++ src/main/java/com/vmware/Employee.java
@@ -1,8 +1,12 @@
 package com.vmware;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Employee {
    private String name;
    private String ssn;
+   
    public void setName(String name) {
       this.name = name;
    }
@@ -12,9 +16,11 @@ public class Employee {
    }
 
 public void setSocialSecurityNumber(String ssn) {
-	this.ssn=ssn;
-	// TODO Auto-generated method stub
-	
+	Pattern pattern = Pattern.compile("\\d{3}-\\d{2}-\\d{4}");
+	Matcher matcher = pattern.matcher(ssn);		
+	if(!(matcher.matches()))
+    throw new IllegalArgumentException(ssn + " is not a valid social security number");
+	this.ssn=ssn;	
 }
 
 public String getSocialSecurityNumber() {
--- src/test/java/com/vmware/EmployeeTest.java
+++ src/test/java/com/vmware/EmployeeTest.java
@@ -29,4 +29,22 @@ public class EmployeeTest {
 		employee.setSocialSecurityNumber("123-45-6708");
 		assertEquals(employee.getSocialSecurityNumber(), "123-45-6708");
 	}
+	
+	/**
+	 * Social Security number must be in social Security format
+	 */
+	@Test
+	public void fix123457_SocialSecurityNumberMustBeACertainFormat(){
+		
+		String badSSN ="RamLikesThe49ers";
+		Employee employee = new Employee();
+		try{
+			employee.setSocialSecurityNumber(badSSN);
+			fail("This Line Should never been reach");
+			
+		}catch (IllegalArgumentException line){
+			assertEquals(line.getMessage(), "RamLikesThe49ers is not a valid social security number");
+		}
+		
+	}
 }
